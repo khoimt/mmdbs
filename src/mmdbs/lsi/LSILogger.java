4e9aabd2c994c9331cba4fcdb44b240104a46085
khoimt <khoimt@live.com>
2014-05-19 21:41:58 +0700

    new log functions

--- src/mmdbs/lsi/LSILogger.java
+++ src/mmdbs/lsi/LSILogger.java
@@ -5,6 +5,7 @@ import java.io.PrintStream;
 import javax.swing.JTextArea;
 
 public class LSILogger {
+
     protected static PrintStream _stream = System.out;
     protected static JTextArea _outputPanel = null;
 
@@ -12,44 +13,55 @@ public class LSILogger {
         _outputPanel = outputPanel;
     }
 
-    
+    public static void print(String msg) {
+        _stream.print(msg);
+        if (_outputPanel != null) {
+            _outputPanel.setText(_outputPanel.getText() + msg);
+        }
+    }
+
     public static void log(String msg) {
         _stream.println(msg);
         if (_outputPanel != null) {
-            _outputPanel.setText(_outputPanel.getText() + "\n" + msg);
+            _outputPanel.setText(_outputPanel.getText() + msg + "\n");
         }
     }
+
     public static void log(int msg) {
         _stream.println(msg);
         if (_outputPanel != null) {
-            _outputPanel.setText(_outputPanel.getText() + "\n" + msg);
+            _outputPanel.setText(_outputPanel.getText() + msg + "\n");
         }
     }
+
     public static void log(double msg) {
         _stream.println(msg);
         if (_outputPanel != null) {
-            _outputPanel.setText(_outputPanel.getText() + "\n" + msg);
+            _outputPanel.setText(_outputPanel.getText() + msg + "\n");
         }
     }
+
     public static void log(float msg) {
         _stream.println(msg);
         if (_outputPanel != null) {
-            _outputPanel.setText(_outputPanel.getText() + "\n" + msg);
+            _outputPanel.setText(_outputPanel.getText() + msg + "\n");
         }
     }
+
     public static void log(char msg) {
         _stream.println(msg);
         if (_outputPanel != null) {
-            _outputPanel.setText(_outputPanel.getText() + "\n" + msg);
+            _outputPanel.setText(_outputPanel.getText() + msg + "\n");
         }
     }
+
     public static void log(int[] msg) {
         StringBuilder str = new StringBuilder();
         str.append('[');
         for (int i = 0; i < msg.length; i++) {
             str.append(msg[i] + " ");
         }
-        str.append("]\n");
+        str.append("]");
         log(str.toString());
     }
 }
